980f3e1b9a24c63b0404e567d8519e589a4567a5
mey <meydetour@gmail.com>
2024-01-09 09:37:56 +0100

    en cour

--- suite.py
+++ suite.py
@@ -1,7 +1,10 @@
 def isgood(result):
-    return 2000 < result < 3000
+    return 2000 < result < 2050
+
+
 def iflow(result):
-    return result<2000
+    return result < 2000
+
 
 def calcul(a, b, puissance):
     result = 0
@@ -11,12 +14,26 @@ def calcul(a, b, puissance):
 
 
 def verify(puissance):
-    liste = []  # renvoie un tuple avec resultat (a,b,puissance)
     a = 1
-    b = 1
-    while isgood(calcul(a,b,puissance)) or iflow(calcul(a,b,puissance))  :
-        print(calcul(a,b,puissance))
+    b = 2
+    for k in range(30):
 
+        print(a, b, isgood(calcul(a, b, puissance)), iflow(calcul(a, b, puissance)))
+        while isgood(calcul(a, b, puissance)) or iflow(calcul(a, b, puissance)):
+            print(a, b, calcul(a, b, puissance))
+            if isgood(calcul(a, b, puissance)):
+                liste.append((a, b, puissance))
+            b += 1
+        print(liste)
+        a += 1
+        b = a + 1
+    return liste
 
+def run():
+    for k in range(11):
+        verify(k)
+    return liste
 
-print(calcul(2, 9, 3))
+liste = []  # renvoie un tuple avec resultat (a,b,puissance)
+
+print(run())
